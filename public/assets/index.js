eb770ec3846521ba5f42f23725529f3dcddf6fb4
Frederik Dudzik <frederik@dudzik.co>
2016-08-27 13:33:06 +0200

    refactor code into different layers

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,49 +1,44 @@
+// glaswasser trinken
+// kegels
+// chinease app to anki
+// anki
+// meditieren
+// training
+// kegels
+// arbeiten
+// schlafen geh routine
+
+// every four days
+// bio trash
+// flowers
+//
+// twice a week
+// wash
+//
+// every week
+// trash
+// vacum
+// shopping
+// pump ball
+// shave
+// clean bathroom
+//
+// once two weeks
+// change bed sheets
+//
+// every two weeks
+// vacum corners
+// cut fingernails
+//
+// once a month
+// wasserkocherentkalken
+// photo review
+// cut footnails
+//
+// every three months
+// change toothbrush head
+
 (function() {
-  var breakfast = [
-    [
-      [ "take out a bowl", "take out a cutting board", "take out small plate", "take out a cup" ],
-      [ "take out a knife", "take out a spoon" ],
-      [ "take out a tea bag & put into cup", "milk", "oat", "banana" ]
-    ],
-    ["put water in the kettle", "put oat in bowl", "put milk into the bowl", "Put the bowl into the microwave for 2 minutes at 700W"],
-    ["pour hot water into the cup", "Put plate on cup", "Set an alarm for 8 minutes"],
-    ["Cut the banana", "Throw the banana peal away", "put banana into the bowl", "stir the oat with the spoon"],
-    ["put away dried dishes"],
-    ["Clean the cutting board", "Clean the knife"],
-    ["put away oat", "put away milk", "put away phone"],
-    ["breathing exercises", "throw tea bag away"],
-    ["Put away phone & eat"],
-    ["clean bowl", "clean spoon", "clean cup", "clean table"]
-  ]
-
-  var store = breakfast
-
-  var index;
-
-  var date = new Date();
-  date.setHours(5);
-
-  if (date > new Date()) {
-    localStorage.removeItem("index")
-    localStorage.removeItem("checkedElements")
-  }
-
-  if(!localStorage.getItem('index')) {
-    index = deepIndex([])
-  }
-  else {
-    index = JSON.parse(localStorage.getItem("index"));
-  }
-
-  var checkedElements;
-  if(!localStorage.getItem('checkedElements')) {
-    checkedElements = []
-  }
-  else {
-    checkedElements = JSON.parse(localStorage.getItem("checkedElements"));
-  }
-
-  var list = document.getElementById("list");
 
   function deepCopy(obj) {
     if (typeof obj == 'object') {
@@ -66,91 +61,222 @@
     return obj;
   }
 
-  function findIndex(index) {
-    return index.reduce(function(a, b) {
-      return a[b] || []
-    }, store)
-  }
-
-  function initFromIndex () {
-    var value = findIndex(index)
-    value.forEach(function (value, i) {
-      if (!checkedElements.includes(i)) {
-        newElement(value, i)
+  if (!Array.prototype.getFirstDeepIndex ) {
+    Array.prototype.getFirstDeepIndex = function(index) {
+      'use strict';
+      if (this == null) {
+        throw new TypeError('Array.prototype.getFirstDeepIndex called on null or undefined');
       }
-    })
-    if(list.childNodes.length === 0) {
-      newElement("no more tasks", 0, true)
-    }
-  }
 
-  function deepIndex (i) {
-    var currentStore = findIndex(i)
-    var newI = []
+      var list = Object(this);
 
-    while(true) {
-      if(typeof currentStore[0] == 'string') {
-        break
+      var currentStore = list.findByIndex(index)
+      var newI = []
+
+      while(true) {
+        if(typeof currentStore[0] == 'string') {
+          break
+        }
+        newI.push(0)
+        currentStore = currentStore[0]
       }
-      newI.push(0)
-      currentStore = currentStore[0]
+      return index.concat(newI)
     }
-    return i.concat(newI)
   }
 
-  function incIndex (i) {
-    i[i.length - 1] = i[i.length - 1] + 1
-    var exists  = findIndex(i).length != 0
-    if (exists) {
-      return deepIndex(i)
-    }
-    if(typeof i.pop() == "undefined" || i.length === 0) {
-      return false
-    }
-    return incIndex(i)
+  if (!Array.prototype.findByIndex) {
+    Array.prototype.findByIndex = function(index) {
+      'use strict';
+      if (this == null) {
+        throw new TypeError('Array.prototype.findByIndex called on null or undefined');
+      }
+
+      var list = Object(this);
+
+      return index.reduce(function(a, b) {
+        return a[b] || []
+      }, list)
+
+    };
   }
 
-  function moveIndex () {
-    var res = incIndex(index)
-    if (res) {
-      index = res
-      localStorage.setItem("index", JSON.stringify(index));
-      checkedElements = []
-      localStorage.setItem("checkedElements", JSON.stringify(checkedElements));
-      initFromIndex()
+  // Store
+
+  var store = [
+    [
+      [ "take out a bowl", "take out a cutting board", "take out small plate", "take out a cup" ],
+      [ "take out a knife", "take out a spoon" ],
+      [ "take out a tea bag & put into cup", "milk", "oat", "banana" ]
+    ],
+    ["put water in the kettle", "put oat in bowl", "put milk into the bowl", "Put the bowl into the microwave for 2 minutes at 700W"],
+    ["pour hot water into the cup", "Put plate on cup", "Set an alarm for 8 minutes"],
+    // ["Cut the banana", "Throw the banana peal away", "put banana into the bowl", "stir the oat with the spoon"],
+    // ["put away dried dishes"],
+    // ["Clean the cutting board", "Clean the knife"],
+    // ["put away oat", "put away milk", "put away phone"],
+    // ["breathing exercises", "throw tea bag away"],
+    // ["Put away phone & eat"],
+    // ["clean bowl", "clean spoon", "clean cup", "clean table"]
+  ]
+
+  class Index {
+
+    constructor (store) {
+      this._store = store
+
+      if(!localStorage.getItem('index')) {
+        this._index = this._store.getFirstDeepIndex([])
+      }
+      else {
+        this._index = JSON.parse(localStorage.getItem("index"));
+      }
+
+    }
+
+    inc (_i) {
+      var i = _i || this._index
+      i[i.length - 1] = i[i.length - 1] + 1
+
+      var tasks = this._store.findByIndex(i)
+      var exists  = tasks.length != 0
+      if (exists) {
+        return this._store.getFirstDeepIndex(i)
+      }
+      if(typeof i.pop() == "undefined" || i.length === 0) {
+        return false
+      }
+      return this.inc(i)
+    }
+
+    move () {
+      var res = this.inc()
+      if (res) {
+        this._index = res
+
+        localStorage.setItem("index", JSON.stringify(this._index));
+
+        _currentElements.reset()
+
+        var value = store.findByIndex(this._index)
+        ul.dispatchEvent(new CustomEvent('new', { 'detail': value }))
+      }
+      else {
+        ul.dispatchEvent(new CustomEvent('new', { 'detail': [] }))
+      }
+    }
+
+    reset () {
+      localStorage.removeItem("index")
+      this._index = store.getFirstDeepIndex([])
+    }
+  }
+
+  class CurrentElements {
+    constructor () {
+      if(!localStorage.getItem('checkedElements')) {
+        this._checkedElements = []
+      }
+      else {
+        this._checkedElements = JSON.parse(localStorage.getItem("checkedElements"));
+      }
+    }
+
+    add (index) {
+      this._checkedElements.push(index)
+      localStorage.setItem("checkedElements", JSON.stringify(this._checkedElements));
+    }
+
+    includes(i) {
+      return this._checkedElements.includes(i)
+    }
+
+    reset () {
+      localStorage.removeItem("checkedElements")
+      this._checkedElements = []
+    }
+  }
+
+  // Service
+
+  class Task {
+
+    constructor() {
+    }
+
+    newDayReset() {
+    }
+
+  }
+
+  var currentTask      = new Task()
+  var _currentElements = new CurrentElements()
+  var _index           = new Index(store)
+
+
+  // View
+
+  var ul = document.getElementById("list");
+
+  function li (childNode) {
+    var element = document.createElement("li");
+    element.appendChild(childNode);
+    return element
+  }
+
+  function taskElement (content, index) {
+    var element = li(document.createTextNode(content))
+    element.onclick = function() {
+      ul.dispatchEvent(new CustomEvent('remove', { 'detail': { task: this, index} }))
+    }
+    return element
+  }
+
+  ul.addEventListener('new', function(e) {
+    var tasks = e.detail
+
+    if(tasks.length === 0) {
+      var element = li(document.createTextNode("no more tasks"))
+      ul.appendChild(element);
+      _currentElements.reset()
+      _index.reset()
     }
     else {
-      newElement("no more tasks", 0, true)
-    }
-  }
-
-  function newElement (content, index, noClick) {
-    var element = document.createElement("li");
-    element.onclick = function() {
-      if(!!noClick) {
-        return
-      }
-
-      checkedElements.push(index)
-      localStorage.setItem("checkedElements", JSON.stringify(checkedElements));
-
-      var self = this
-      var parentNode = this.parentNode
-
-      this.classList.add('removed')
-
-      setTimeout(function() {
-        parentNode.removeChild(self);
-        if(!parentNode.childNodes.length) {
-          moveIndex()
+      tasks.forEach(function (value, i) {
+        if (!_currentElements.includes(i)) {
+          var element = taskElement(value, i)
+          ul.appendChild(element);
         }
-      }, 150)
+      })
     }
+  })
 
-    // element.classList.add('active')
-    element.appendChild(document.createTextNode(content));
-    list.appendChild(element);
+  ul.addEventListener('remove', function(e) {
+    var task  = e.detail.task
+    var index = e.detail.index
+
+    _currentElements.add(index)
+
+    task.classList.add('removed')
+
+    setTimeout(function() {
+      ul.removeChild(task);
+      if(!ul.childNodes.length) {
+        _index.move()
+      }
+    }, 150)
+  })
+
+  // Init
+
+  // reset if new day
+  var date = new Date();
+  date.setHours(5);
+  if (date > new Date()) {
+    _currentElements.reset()
+    _index.reset()
   }
 
-  initFromIndex()
+  var value = store.findByIndex(_index._index)
+  ul.dispatchEvent(new CustomEvent('new', { 'detail': value }))
+
 })()
